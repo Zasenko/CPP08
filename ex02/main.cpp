ef7b7c824ce621f5b0347098c8fb55170c41b9f4
Dmitry Zasenko <dzasenko@c3r5p3.42vienna.com>
2025-10-16 13:01:51 +0200

    fixing

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -2,51 +2,85 @@
 #include <iostream>
 
 int main() {
-    
-    // MutantStack<int> mstack;
-    // std::cout << mstack.top() << std::endl;
-    // std::cout << mstack.size() << std::endl;
-    // std::cout << "--------" << std::endl;
-
-    // mstack.push(5);
-    // std::cout << mstack.top() << std::endl;
-    // std::cout << mstack.size() << std::endl;
-    // std::cout << "--------" << std::endl;
-
-    // mstack.push(17);
-    // std::cout << mstack.top() << std::endl;
-    // std::cout << mstack.size() << std::endl;
-    // std::cout << "--------" << std::endl;
-
-    // mstack.pop();
-    // std::cout << mstack.top() << std::endl;
-    // std::cout << mstack.size() << std::endl;
-    // std::cout << "--------" << std::endl;
-
+   
     MutantStack<int> mstack;
+
     mstack.push(5);
     mstack.push(17);
+
     std::cout << mstack.top() << std::endl;
+
     mstack.pop();
+
     std::cout << mstack.size() << std::endl;
+
     mstack.push(3);
     mstack.push(5);
     mstack.push(737);
     mstack.push(0);
+
     MutantStack<int>::iterator it = mstack.begin();
     MutantStack<int>::iterator ite = mstack.end();
+
     ++it;
     --it;
+
     while (it != ite)
     {
         std::cout << *it << std::endl;
         ++it;
     }
-    mstack.push(666);
+
     std::stack<int> s(mstack);
-    std::cout << "----" << std::endl;
-    std::cout << s.top() << std::endl;
+
+    std::cout << "--------- my tests: ---------" << std::endl;
+
+    std::cout << "mstack top: " << mstack.top() << std::endl;
+    std::cout << "mstack size: " <<  mstack.size() << std::endl;
+
+    MutantStack<int> mstackCopy(mstack);
+    std::cout << "copy top: " <<  mstack.top() << std::endl;
+    std::cout << "copy size: " <<  mstack.size() << std::endl;
+
+    const MutantStack<int> mstackAs = mstack;
+    std::cout << "as top: " <<  mstackAs.top() << std::endl;
+    std::cout << "as size: " <<  mstackAs.size() << std::endl;
 
     return 0;
 }
 
+// #include <list>
+// int main() {
+    
+//     std::list<int> mstack;
+
+//     mstack.push_back(5);
+//     mstack.push_back(17);
+
+//     std::cout << mstack.back() << std::endl;
+
+//     mstack.pop_back();
+
+//     std::cout << mstack.size() << std::endl;
+
+//     mstack.push_back(3);
+//     mstack.push_back(5);
+//     mstack.push_back(737);
+//     mstack.push_back(0);
+
+//     std::list<int>::iterator it = mstack.begin();
+//     std::list<int>::iterator ite = mstack.end();
+
+//     ++it;
+//     --it;
+
+//     while (it != ite)
+//     {
+//         std::cout << *it << std::endl;
+//         ++it;
+//     }
+
+//     std::list<int> s(mstack);
+
+//     return 0;
+// }
